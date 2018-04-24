3ea112eaf125595a9ab982e54fe5723fb9fe15a6
slontia <654867229@qq.com>
2018-04-24 18:18:30 +0800

    supply annotation to main.h

--- code/main.h
+++ code/main.h
@@ -1,3 +1,3 @@
-#define NEW_TAR 1
-#define AUTO_TEST 0
-#define LAST_AUTO_TEST 0
+#define NEW_TAR 1           // optimization option
+#define AUTO_TEST 0         // generate C0 test code and compile
+#define LAST_AUTO_TEST 0    // compile last C0 test code
